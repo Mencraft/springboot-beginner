b3c6b5bf442666c3b57f99d06bfb1528a56036b5
mencraft <ekemboy@gmail.com>
2023-12-21 14:45:28 +0800

    add api key secruity

--- src/main/java/com/example/cruddemo/controller/homeController.java
+++ src/main/java/com/example/cruddemo/controller/homeController.java
@@ -1,12 +1,16 @@
 package com.example.cruddemo.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/home")
 public class homeController {
-   @GetMapping("/home")
-    public String homeEndpoint() {
-        return "Baeldung !";
+
+  @GetMapping
+  public String homeEndpoint() {
+        return "Hello From CRUDBY!";
     }
 }
